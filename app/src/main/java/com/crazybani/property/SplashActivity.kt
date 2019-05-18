3f9eb6679dddcda525c460fadc51312d54031e68
Subhan Ali <subhanali.softengg@gmail.com>
2019-05-19 00:13:00 +0400

    Navigation Added: SplashActivity to MainActivity

--- app/src/main/java/com/crazybani/property/SplashActivity.kt
+++ app/src/main/java/com/crazybani/property/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.crazybani.property
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var mDelayHandler: Handler;
+    private val DELAY = 3000L
+    private var mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler()
+        mDelayHandler.postDelayed(mRunnable, DELAY)
     }
 }
