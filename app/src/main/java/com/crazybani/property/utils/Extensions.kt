dd2e278520b6a2a6d0f126bcbcbfd723d428b9eb
Subhan Ali <subhanali.softengg@gmail.com>
2019-05-22 23:03:36 +0400

    Inline method added for FragmentTransition : Extensions

--- app/src/main/java/com/crazybani/property/utils/Extensions.kt
+++ app/src/main/java/com/crazybani/property/utils/Extensions.kt
@@ -1,7 +1,19 @@
 package com.crazybani.property.utils
 
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
 /** Convenience for callbacks/listeners whose return value indicates an event was consumed. */
 inline fun consume(f: () -> Unit): Boolean{
     f()
     return true
+}
+
+/**
+ * Allows calls like
+ *
+ * `supportFragmentManager.inTransaction { add(...) }`
+ */
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
 }
