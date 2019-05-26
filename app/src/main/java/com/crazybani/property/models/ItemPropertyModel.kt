3f829def793bc8785f365d2ad7a93589df4ecbbe
Subhan Ali <subhanali.softengg@gmail.com>
2019-05-26 23:49:32 +0400

    Property Data Model added

---
+++ app/src/main/java/com/crazybani/property/models/ItemPropertyModel.kt
@@ -0,0 +1,10 @@
+package com.crazybani.property.models
+
+
+data class PropertyModel(var propertyImageUrl: String,
+                         var propertyPrice: String,
+                         var propertyPriceDuration: String,
+                         var propertyType: String,
+                         var numOfRooms: String,
+                         var numOfBaths: String,
+                         var address: String)
