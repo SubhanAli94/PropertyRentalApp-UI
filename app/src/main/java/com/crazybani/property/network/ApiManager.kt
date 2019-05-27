ec508afc7fd5295469269e6790fe3b020cd980ff
Subhan Ali <subhanali.softengg@gmail.com>
2019-05-27 22:51:05 +0400

    Init Retrofit

--- app/src/main/java/com/crazybani/property/network/ApiManager.kt
+++ app/src/main/java/com/crazybani/property/network/ApiManager.kt
@@ -3,29 +3,29 @@ package com.crazybani.property.network
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiManager{
+    private val BASE_URL = "";
     private var retrofit: Retrofit? = null
-//    private var apiService: ApiInterface? = null
 
     private fun initRetrofitInstance() {
-//        if (retrofit == null) {
-//
-//            var interceptor = HttpLoggingInterceptor()
-//            interceptor.level = HttpLoggingInterceptor.Level.BODY
-//
-//            var client = OkHttpClient.Builder()
-//            client.addInterceptor(interceptor)
-//
-//            retrofit = Retrofit.Builder()
-//                .baseUrl(NetworkConstants.BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                .client(client.build())
-//                .build()
-//
-//        }
-//        apiService = retrofit!!.create(ApiInterface::class.java)
+        if (retrofit == null) {
+
+            var interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            var client = OkHttpClient.Builder()
+            client.addInterceptor(interceptor)
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client.build())
+                .build()
+
+        }
     }
 }
