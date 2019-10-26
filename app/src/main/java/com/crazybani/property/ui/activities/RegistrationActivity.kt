ae09f1260dfefbe547f88a779bb3365cb126ec0e
Subhan Ali <subhanali.softengg@gmail.com>
2019-10-27 01:03:26 +0400

    RegistrationActivity | UI Completed

--- app/src/main/java/com/crazybani/property/ui/activities/RegistrationActivity.kt
+++ app/src/main/java/com/crazybani/property/ui/activities/RegistrationActivity.kt
@@ -1,7 +1,9 @@
 package com.crazybani.property.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.crazybani.property.R
 
 class RegistrationActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+    }
+
+    fun backButtonClicked(view: View) {
+        finish()
+    }
+    fun navigateToHomeActivity(view: View) {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finishAffinity()
     }
 }
